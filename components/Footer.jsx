1296239bf2e26d9ddcb721e1a96419c5edebe9d8
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-07-23 22:49:41 -0500

    add footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,39 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+
+const LINKS = [
+  {
+    title: "Source",
+    href: "https://github.com/denoland/fresh",
+  },
+  {
+    title: "License",
+    href: "https://github.com/denoland/fresh/blob/main/LICENSE",
+  },
+  {
+    title: "Code of Conduct",
+    href: "https://github.com/denoland/fresh/blob/main/CODE_OF_CONDUCT.md",
+  },
+];
+
+export default function Footer() {
+  const footer = tw`border(t-2 gray-200) bg-gray-100 h-32 flex flex-col gap-4 justify-center`;
+  const inner = tw`mx-auto max-w-screen-lg flex items-center justify-center gap-8`;
+  const linkStyle = tw`text-gray-600 hover:underline`;
+  const copyright = tw`text(gray-600 center)`;
+  return (
+    <footer class={footer}>
+      <div class={inner}>
+        {LINKS.map((link) => (
+          <a href={link.href} class={linkStyle}>
+            {link.title}
+          </a>
+        ))}
+      </div>
+      <div class={copyright}>
+        <span>© {new Date().getFullYear()} the undead authors</span>
+      </div>
+    </footer>
+  );
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -3,6 +3,7 @@ import { Fragment, h } from "preact";
 import { tw } from "@twind";
 import Counter from "../islands/Counter.tsx";
 import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
 
 export default function Home() {
   return (
@@ -14,9 +15,13 @@ export default function Home() {
           height="100px"
           alt="the fresh logo: a sliced lemon dripping with juice"
         />
-        <p class={tw`my-6`}>Welcome to Riefer's Little Big Secret API</p>
+        <p class={tw`my-6`}>
+          Welcome to Riefer's Little Big Secret API - The key to an unknown
+          world
+        </p>
         <Counter start={3} />
       </div>
+      <Footer />
     </Fragment>
   );
 }
