febdeaecd2790b0dc8811d139d55f7097332b0ac
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-08-26 22:46:15 -0500

    get environmental variable

--- utils/database-connection.ts
+++ utils/database-connection.ts
@@ -2,7 +2,7 @@ import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 import * as postgres from "https://deno.land/x/postgres@v0.16.1/mod.ts";
 
 // Get the connection string from the environment variable "DATABASE_URL"
-const databaseUrl = config().DATABASE_URL;
+const databaseUrl = config().DATABASE_URL || Deno.env.get("DATABASE_URL");
 
 // Create a database pool with three connections that are lazily established
 const dbPool = new postgres.Pool(databaseUrl, 3, true);
