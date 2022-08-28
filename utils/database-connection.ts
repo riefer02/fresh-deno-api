91180afe3082af4cc7be2afda084a51f269a8d19
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-08-28 10:21:10 -0500

    add dbConnection back

--- utils/database-connection.ts
+++ utils/database-connection.ts
@@ -1,13 +1,13 @@
-// import { config } from "https://deno.land/x/dotenv/mod.ts";
-// import * as postgres from "https://deno.land/x/postgres@v0.16.1/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+import * as postgres from "https://deno.land/x/postgres@v0.16.1/mod.ts";
 
-// // Get the connection string from the environment variable "DATABASE_URL"
-// const databaseUrl = config().DATABASE_URL || Deno.env.get("DATABASE_URL");
+// Get the connection string from the environment variable "DATABASE_URL"
+const databaseUrl = config().DATABASE_URL || Deno.env.get("DATABASE_URL");
 
-// // Create a database pool with three connections that are lazily established
-// const dbPool = new postgres.Pool(databaseUrl, 3, true);
+// Create a database pool with three connections that are lazily established
+const dbPool = new postgres.Pool(databaseUrl, 3, true);
 
-// // Connect to the database
-// const dbConn = await dbPool.connect();
+// Connect to the database
+const dbConn = await dbPool.connect();
 
-// export default dbConn;
+export default dbConn;
