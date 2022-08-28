b589b84a184eca5d04341b22bbee91935caf27cf
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-08-28 10:19:52 -0500

    add gfm css and markdown logic back

--- routes/gfm.css.tsx
+++ routes/gfm.css.tsx
@@ -1,48 +1,48 @@
-// import { Handlers } from "$fresh/server.ts";
-// import { gfm } from "../utils/markdown.ts";
+import { Handlers } from "$fresh/server.ts";
+import { gfm } from "../utils/markdown.ts";
 
-// // ToResearch: hash the file and use the hash as the filename, and serve
-// // with high-cacheability headers.
+// ToResearch: hash the file and use the hash as the filename, and serve
+// with high-cacheability headers.
 
-// const CSS = `${gfm.CSS}
+const CSS = `${gfm.CSS}
 
-// ol.nested {
-// 	counter-reset: item;
-// }
+ol.nested {
+	counter-reset: item;
+}
 
-// ol.nested li {
-// 	display: block;
-// }
+ol.nested li {
+	display: block;
+}
 
-// ol.nested li:before {
-// 	font-feature-settings: "kern" 1, "tnum" 1;
-// 	-webkit-font-feature-settings: "kern" 1, "tnum" 1;
-// 	-ms-font-feature-settings: "kern" 1, "tnum" 1;
-// 	-moz-font-feature-settings: "kern" 1, "tnum" 1;
-// 	content: counters(item, ".") ". ";
-// 	counter-increment: item;
-// }
+ol.nested li:before {
+	font-feature-settings: "kern" 1, "tnum" 1;
+	-webkit-font-feature-settings: "kern" 1, "tnum" 1;
+	-ms-font-feature-settings: "kern" 1, "tnum" 1;
+	-moz-font-feature-settings: "kern" 1, "tnum" 1;
+	content: counters(item, ".") ". ";
+	counter-increment: item;
+}
 
-// .markdown-body ul {
-//   list-style: disc;
-// }
+.markdown-body ul {
+  list-style: disc;
+}
 
-// .markdown-body ol {
-//   list-style: numeric;
-// }
+.markdown-body ol {
+  list-style: numeric;
+}
 
-// .toggle:checked + .toggled {
-// 	display: block;
-// }
-// `;
+.toggle:checked + .toggled {
+	display: block;
+}
+`;
 
-// export const handler: Handlers = {
-//   GET: () => {
-//     return new Response(CSS, {
-//       headers: {
-//         "content-type": "text/css",
-//         "cache-control": "public, max-age=31536000, immutable",
-//       },
-//     });
-//   },
-// };
+export const handler: Handlers = {
+  GET: () => {
+    return new Response(CSS, {
+      headers: {
+        "content-type": "text/css",
+        "cache-control": "public, max-age=31536000, immutable",
+      },
+    });
+  },
+};
