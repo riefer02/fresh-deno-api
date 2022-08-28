fd8288e50f898a32551e46b48ca50074dcf42bdb
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-08-28 10:14:54 -0500

    comment out component

--- routes/index.tsx
+++ routes/index.tsx
@@ -138,7 +138,7 @@ export default function Home() {
         Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
         file, and refresh.
       </p>
-      <Counter start={3} />
+      {/* <Counter start={3} /> */}
     </div>
   );
 }
