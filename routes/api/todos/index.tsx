7704f61c91c275e77c5e5a9d94c865c6500bed4a
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-08-28 10:27:36 -0500

    add todo index api route back

--- routes/api/todos/index.tsx
+++ routes/api/todos/index.tsx
@@ -1,58 +1,58 @@
-// /** @jsx h */
-// import { h } from "preact";
-// import { Handlers } from "$fresh/server.ts";
-// import dbConn from "../../../utils/database-connection.ts";
+/** @jsx h */
+import { h } from "preact";
+import { Handlers } from "$fresh/server.ts";
+import dbConn from "../../../utils/database-connection.ts";
 
-// export const handler: Handlers = {
-//   async GET(req, ctx) {
-//     try {
-//       const results = await dbConn.queryObject`
-//         SELECT * FROM public.todos
-//       `;
-//       const todos = results.rows;
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    try {
+      const results = await dbConn.queryObject`
+        SELECT * FROM public.todos
+      `;
+      const todos = results.rows;
 
-//       BigInt.prototype.toJSON = function () {
-//         return this.toString();
-//       };
+      BigInt.prototype.toJSON = function () {
+        return this.toString();
+      };
 
-//       return new Response(JSON.stringify({ todos }), {
-//         status: 200,
-//         statusText: "OK",
-//         headers: { "Content-Type": "application/json" },
-//       });
-//     } catch (err) {
-//       return new Response(`${err.message}`, { status: 404 });
-//     } finally {
-//       dbConn.release();
-//     }
-//   },
+      return new Response(JSON.stringify({ todos }), {
+        status: 200,
+        statusText: "OK",
+        headers: { "Content-Type": "application/json" },
+      });
+    } catch (err) {
+      return new Response(`${err.message}`, { status: 404 });
+    } finally {
+      dbConn.release();
+    }
+  },
 
-//   async POST(req, ctx) {
-//     try {
-//       const { title } = await req.json();
+  async POST(req, ctx) {
+    try {
+      const { title } = await req.json();
 
-//       if (!title)
-//         return new Response(
-//           JSON.stringify({
-//             message: "No title property found in request body",
-//           }),
-//           {
-//             status: 404,
-//           }
-//         );
+      if (!title)
+        return new Response(
+          JSON.stringify({
+            message: "No title property found in request body",
+          }),
+          {
+            status: 404,
+          }
+        );
 
-//       await dbConn.queryObject`
-//       INSERT INTO todos (title) VALUES (${title})
-//     `;
+      await dbConn.queryObject`
+      INSERT INTO todos (title) VALUES (${title})
+    `;
 
-//       return new Response(
-//         JSON.stringify({ message: "New todo added to database" }),
-//         { status: 201, statusText: "Todo Created" }
-//       );
-//     } catch (err) {
-//       return new Response(`${err.message}`, { status: 500 });
-//     } finally {
-//       dbConn.release();
-//     }
-//   },
-// };
+      return new Response(
+        JSON.stringify({ message: "New todo added to database" }),
+        { status: 201, statusText: "Todo Created" }
+      );
+    } catch (err) {
+      return new Response(`${err.message}`, { status: 500 });
+    } finally {
+      dbConn.release();
+    }
+  },
+};
