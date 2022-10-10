aa327b9dad7d59c8d60c56009fc693a818a323fb
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-10-10 13:47:05 -0500

    send debugger up

--- utils/database-pool.ts
+++ utils/database-pool.ts
@@ -4,7 +4,7 @@ import * as postgres from "https://deno.land/x/postgres@v0.16.1/mod.ts";
 // Get the connection string from the environment variable "DATABASE_URL"
 const databaseUrl = Deno.env.get("DATABASE_URL")! || config().DATABASE_URL;
 
-// Create a database pool with three connections that are lazily established
+// Create a database pool with twelve connections that are lazily established
 const dbPool = new postgres.Pool(databaseUrl, 12, true);
 
 export default dbPool;
