eef0648d030af272316733b5918da43c7636045b
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-10-23 11:11:29 -0600

    upgrade postgres import 'database-pool'

--- utils/database-pool.ts
+++ utils/database-pool.ts
@@ -1,5 +1,5 @@
 import { config } from "https://deno.land/x/dotenv/mod.ts";
-import * as postgres from "https://deno.land/x/postgres@v0.16.1/mod.ts";
+import * as postgres from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
 // Get the connection string from the environment variable "DATABASE_URL"
 const databaseUrl = Deno.env.get("DATABASE_URL")! || config().DATABASE_URL;
